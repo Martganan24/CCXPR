5e979b83c8dc306e2500fbb76b6c9f2ce916bb12
martganan24 <mart.ganan24@gmail.com>
2025-03-13 19:43:32 +0800

    f wallet

--- cecxai-trading/src/components/OrderForm.js
+++ cecxai-trading/src/components/OrderForm.js
@@ -45,7 +45,6 @@ function OrderForm() {
   const [isProcessing, setIsProcessing] = useState(false); // To track if a trade is in process
   const [result, setResult] = useState(""); // To store the result (win/loss)
   const [popupVisible, setPopupVisible] = useState(false); // Show the countdown popup
-  const [timeLeft, setTimeLeft] = useState(2); // Timer state for countdown (2 seconds for testing)
   const [showCloseButton, setShowCloseButton] = useState(false); // State to control close button visibility
 
   // Decrease amount (Min: 0)
